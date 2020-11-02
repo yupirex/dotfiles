7b98ef6a2ae37c991f6565c02778e91a0cce89b1
YupiRex <yupirex@gmail.com>
2020-11-02 23:19:31 +0200

    +25 all fixed

--- bashpromt/install.sh
+++ bashpromt/install.sh
@@ -1,8 +1,7 @@
 #!/usr/bin/env sh
 echo "Write https://raw.githubusercontent.com/yupirex/dotfiles/master/bashpromt/.bashpromt -> ~/.bashpromt"
 wget -qO ~/.bashpromt https://raw.githubusercontent.com/yupirex/dotfiles/master/bashpromt/.bashpromt
-grep -q ".bashpromt" "$HOME"/.bashrc
-if [ "$?" -eq 0  ]; then
+if [ "$(command grep -q ".bashpromt" "$HOME"/.bashrc)" -eq 0  ]; then
     echo ".bashpromt already added in ~/.bashrc"
 else echo "source ~/.bashpromt" >> ~/.bashrc
     echo "Line added: echo source ~/.bashpromt >> ~/.bashrc"

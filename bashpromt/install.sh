99e37393f56b43d939a8474f359563ba912f5178
YupiRex <yupirex@gmail.com>
2020-11-02 23:01:59 +0200

    install on dash

--- bashpromt/install.sh
+++ bashpromt/install.sh
@@ -1,7 +1,11 @@
-#!/usr/share/env bash
+#!/usr/bin/env sh
 echo "Write https://raw.githubusercontent.com/yupirex/dotfiles/master/bashpromt/.bashpromt -> ~/.bashpromt"
 wget -qO ~/.bashpromt https://raw.githubusercontent.com/yupirex/dotfiles/master/bashpromt/.bashpromt
+grep -q ".bashpromt" "$HOME"/.bashrc
+if [ "$?" -eq 0  ]; then
+    echo ".bashpromt already added in ~/.bashrc"
+else echo "source ~/.bashpromt" >> ~/.bashrc
+    echo "Line added: echo source ~/.bashpromt >> ~/.bashrc"
+fi
 
-grep ".bashpromt" "$HOME"/.bashrc >/dev/null && echo ".bashpromt already added in ~/.bashrc" || { echo "source ~/.bashpromt" >> ~/.bashrc; echo "Line added: echo source ~/.bashpromt >> ~/.bashrc"; }
-    
 echo "Completed!"

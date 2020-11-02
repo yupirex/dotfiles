f813d0b9d735c72d4dc760225c326dca710081a4
regemdeu <60338621+regemdeu@users.noreply.github.com>
2020-11-02 22:19:03 +0300

    Update install.sh

--- bashpromt/install.sh
+++ bashpromt/install.sh
@@ -1,9 +1,11 @@
 #!/usr/share/env bash
-wget -qO ~/.bashpromt https://raw.githubusercontent.com/yupirex/dotfiles/master/bashpromt/.bashpromt &&
-echo "Write https://raw.githubusercontent.com/yupirex/dotfiles/master/bashpromt/.bashpromt -> ~/.bashpromt" &&
+echo "Write https://raw.githubusercontent.com/yupirex/dotfiles/master/bashpromt/.bashpromt -> ~/.bashpromt"
+wget -qO ~/.bashpromt https://raw.githubusercontent.com/yupirex/dotfiles/master/bashpromt/.bashpromt
+
 (grep ".bashpromt" ~/.bashrc &>/dev/null &&
     echo "\"source ~/.bashpromt\" already added in ~/.bashrc" )|| 
     (echo "source ~/.bashpromt" >> ~/.bashrc && 
     echo "Line added" &&
     $0)
+    
 echo "Completed!"

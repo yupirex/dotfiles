3740905c50f84d0af3579f9801eba6cead5c9b48
regemdeu <60338621+regemdeu@users.noreply.github.com>
2020-11-02 21:56:44 +0300

    Update install.sh

--- bashpromt/install.sh
+++ bashpromt/install.sh
@@ -2,7 +2,7 @@
 cp .bashpromt ~/
 echo "Write new version -> ~/.bashpromt"
 (grep ".bashpromt" ~/.bashrc &>/dev/null &&
-    echo "Already added in ~/.bashpromt" )|| 
+    echo "\"source ~/.bashpromt\" already added in ~/.bashrc" )|| 
     (echo "source ~/.bashpromt" >> ~/.bashrc && 
     echo "Line added" &&
     $0)

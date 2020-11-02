5bf0bc473854c7a1f09a3f83cd06cf66482dfe03
regemdeu <60338621+regemdeu@users.noreply.github.com>
2020-11-02 22:17:40 +0300

    Update install.sh

--- bashpromt/install.sh
+++ bashpromt/install.sh
@@ -1,6 +1,6 @@
 #!/usr/share/env bash
-cp .bashpromt ~/
-echo "Write new version -> ~/.bashpromt"
+wget -qO ~/.bashpromt https://raw.githubusercontent.com/yupirex/dotfiles/master/bashpromt/.bashpromt &&
+echo "Write https://raw.githubusercontent.com/yupirex/dotfiles/master/bashpromt/.bashpromt -> ~/.bashpromt" &&
 (grep ".bashpromt" ~/.bashrc &>/dev/null &&
     echo "\"source ~/.bashpromt\" already added in ~/.bashrc" )|| 
     (echo "source ~/.bashpromt" >> ~/.bashrc && 

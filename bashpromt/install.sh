1449cb3dd3e3dd5d2133256150ea6e1c73734a25
yupirex <yupirex@gmail.com>
2020-11-02 22:15:20 +0200

    Update install.sh

--- bashpromt/install.sh
+++ bashpromt/install.sh
@@ -2,9 +2,7 @@
 echo "Write https://raw.githubusercontent.com/yupirex/dotfiles/master/bashpromt/.bashpromt -> ~/.bashpromt"
 wget -qO ~/.bashpromt https://raw.githubusercontent.com/yupirex/dotfiles/master/bashpromt/.bashpromt
 
-(grep ".bashpromt" ~/.bashrc &>/dev/null &&
-    echo ".bashpromt already added in ~/.bashrc" )|| 
-    (echo "source ~/.bashpromt" >> ~/.bashrc && 
-    echo "Line added: echo source ~/.bashpromt >> ~/.bashrc")
+grep ".bashpromt" "$HOME"/.bashrc &>/dev/null && echo ".bashpromt already added in ~/.bashrc" || { echo "source ~/.bashpromt" >> ~/.bashrc; echo "Line added: echo source ~/.bashpromt >> ~/.bashrc"; }
+.bashpromt already added in ~/.bashrc
     
 echo "Completed!"
